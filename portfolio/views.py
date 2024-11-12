f8e4c143d2b1b8df75b807e2eaa9ce57d3d84b94
TecnoNetPC <tecnonetpcsc@gmail.com>
2024-11-12 18:08:00 +0100

    blogs

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Project
+from blog.models import Post
 
 
 def home(request):
     projects = Project.objects.all()
+    posts = Post.objects.all()
 
-    return render(request, 'home.html', {'projects': projects})
+    return render(request, 'home.html', {
+        'projects': projects,
+        'posts': posts
+        })
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, '/blog/templates/post_detail.html', {'post': post})
